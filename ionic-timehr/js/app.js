3fde3c51f0e69758738bc2495d25d136841a8a37
sunyang3721 <274507284@qq.com>
2015-08-11 14:25:47 +0800

    jobs timehr add

---
+++ ionic-timehr/js/app.js
@@ -0,0 +1,91 @@
+angular.module('kuwuya', ['ionic'])
+.config(function($ionicConfigProvider) {
+  $ionicConfigProvider.platform.ios.tabs.style('standard'); 
+  $ionicConfigProvider.platform.ios.tabs.position('bottom');
+  $ionicConfigProvider.platform.android.tabs.style('standard');
+  $ionicConfigProvider.platform.android.tabs.position('standard');
+
+  $ionicConfigProvider.platform.ios.navBar.alignTitle('center'); 
+  $ionicConfigProvider.platform.android.navBar.alignTitle('center');
+
+  $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-back');
+  $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-ios-arrow-back');  
+
+  $ionicConfigProvider.platform.ios.views.transition('ios'); 
+  $ionicConfigProvider.platform.android.views.transition('android');
+
+  //$ionicConfigProvider.views.swipeBackEnabled(false); //禁止侧滑后退事件
+})
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('eventmenu', {
+      url: "/event",
+      abstract: true,
+      templateUrl: "templates/event-menu.html"
+    })
+    .state('eventmenu.home', {
+      url: "/home",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/home.html"
+        }
+      }
+    })
+    .state('jobmenu', {
+      url: "/jobs",
+      abstract: true,
+      templateUrl: "templates/job-menu.html"
+    })
+    .state('jobmenu.job', {
+      url: "/job",
+      views: {
+        'menuJob' :{
+          templateUrl: "./job.html"
+        }
+      }
+    })
+    .state('jobmenu.joblist',{
+      url:"/joblist",
+      views: {
+        'menuJob' :{
+          templateUrl: "./joblist.html"
+        }
+      }
+    })
+    .state('eventmenu.lietou', {
+      url: "/lietou",
+      views: {
+        'menuContent' :{
+          templateUrl: "./lietou.html"
+          //controller: "CheckinCtrl"
+        }
+      }
+    })
+    .state('eventmenu.css_utility',{   //css 文件夹
+      url:"/css/utility",
+      views:{
+        'menuContent' :{
+          templateUrl: "./css/utility.html"
+        }
+      }
+    })
+    .state('eventmenu.js_ion-header-bar',{   //js 文件夹
+      url:"/js/ion-header-bar",
+      views:{
+        'menuContent' :{
+          templateUrl: "./js/ion-header-bar.html"
+        }
+      }
+    })
+  
+ $urlRouterProvider.otherwise("/event/home");
+})
+
+.controller('MainCtrl',function ($scope,$ionicHistory) {
+    $scope.warn_right = function () {
+        history.back();
+        //$ionicHistory.goBack();
+    };
+    $scope.nav_header = '腾驹达官网';
+  });
