755229740e051894a86a3213be46d498b156b9f1
y470_sunyang <274507284@qq.com>
2015-08-29 12:42:45 +0800

    待开发

--- ionic-timehr/js/server/namecardServer.js
+++ ionic-timehr/js/server/namecardServer.js
@@ -1,5 +1,6 @@
 var namecard = angular.module('kuwuya.namecardServer', []);
 	namecard.controller('namecardCtrl',function ($rootScope,$timeout) {
 		$rootScope.data = '能显示控制吗';
+		//待开发 用来显示猎头名片 利用$http json格式  
 
 	})
